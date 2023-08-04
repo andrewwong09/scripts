8d00759f9450eb017d40ad0979a44019ca8aee60
andrew <andrewwong09@gmail.com>
2023-08-03 17:04:02 -0700

    threaded working

--- cv_cap/DisplayImage.cpp
+++ cv_cap/DisplayImage.cpp
@@ -10,47 +10,49 @@ using namespace cv;
 using namespace std;
 using namespace std::chrono;
 
-void cap_read(VideoCapture cap) {
-	cap.read()
-}
-
-int main(int, char**) {
-	Mat frame;
+void cap_read(Mat* frame, int* count) {
 	//--- INITIALIZE VIDEOCAPTURE
 	const char* gst_launch_str = "tcambin tcam-properties=tcam,ExposureAuto=Off,serial=46810510,ExposureTime=10000"
 				     " ! video/x-raw,format=BGRx,width=3072,height=2048,framerate=60/1 "
 				     " ! appsink";
-	char img_name_buffer[100];
 	VideoCapture cap(gst_launch_str, CAP_GSTREAMER);
 
 	if (!cap.isOpened()) {
 		cerr << "ERROR! Unable to open camera\n";
-		return -1;
+		return;
 	}
-	cout << "Start grabbing" << endl
-		<< "Press any key to terminate" << endl;
-	//std::thread t1 (cap_read, cap)
-
-	for (int i=0; i < 100; i++) {
-		auto start = high_resolution_clock::now();
-		cap.read(frame);
-		// check if we succeeded
-		if (frame.empty()) {
+	for(int i=0;i < 400; i++) {
+		cap.read(*frame);
+		if (frame->empty()) {
 			cerr << "ERROR! blank frame grabbed\n";
-			break;
 		} else {
-			sprintf(img_name_buffer, "./display_img%03d.bmp", i);
-			imwrite(img_name_buffer, frame);
+			*count = *count + 1;
 		}
-		// show live and wait for a key with timeout long enough to show images
-		// imshow("Live", frame);
-		auto stop = high_resolution_clock::now();
-		auto duration = duration_cast<microseconds>(stop - start);
-
-		cout << i << ": " << duration.count() << " microseconds" << endl;
-		//if (waitKey(5) >= 0)
-		//	break;
 	}
+}
+
+int main(int, char**) {
+	Mat frame;
+	int shared_count = 0;
+	int count = 0;
+	std::thread t1 (cap_read, &frame, &shared_count);
+	char img_name_buffer[100];
+	auto start = high_resolution_clock::now();
+	for (int i=0; i < 100; i++) {
+		// check if we succeeded
+		if (count != shared_count) {
+			count = shared_count;
+			sprintf(img_name_buffer, "./display_img%03d_%03d.jpeg", i, count);
+			imwrite(img_name_buffer, frame);
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
+			cout << i << ": " << duration.count() << " microseconds" << endl;
+			start = stop;
+		} else {
+			i--;
+		}
+	}
+	t1.join();
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
 }
