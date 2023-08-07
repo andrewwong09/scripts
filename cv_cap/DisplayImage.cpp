30e8b790d55bf858d482a3a37fde4b382fe8dc50
andrew <andrew@neuralink.com>
2023-08-07 10:58:54 -0700

    YAML Read integrated.

--- cv_cap/DisplayImage.cpp
+++ cv_cap/DisplayImage.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <iostream>
 #include <stdio.h>
+#include <assert.h>
+#include "yaml-cpp/yaml.h"
 
 #include <opencv2/core.hpp>
 #include "opencv2/opencv.hpp"
@@ -14,9 +16,9 @@ using namespace std::chrono;
 
 void cap_read(Mat* frame, int* count, bool* stop) {
 	const char* gst_launch_str = "tcambin tcam-properties=tcam,ExposureAuto=Off,GainAuto=Off,"
-				     "serial=46810510,ExposureTime=10000,Gain=10"
-				     " ! video/x-raw,format=BGRx,width=3072,height=2048,framerate=60/1"
-				     " ! appsink";
+		"serial=46810510,ExposureTime=10000,Gain=20"
+		" ! video/x-raw,format=BGRx,width=3072,height=2048,framerate=60/1"
+		" ! appsink";
 	cv::VideoCapture cap(gst_launch_str, CAP_GSTREAMER);
 
 	if (!cap.isOpened()) {
@@ -36,7 +38,7 @@ void cap_read(Mat* frame, int* count, bool* stop) {
 void display(Mat* frame, int* count, bool* stop) {
 	int local_count = 0;
 	cv::Mat resized_display_img;
-	
+
 	cv::namedWindow("Display", WINDOW_AUTOSIZE);
 	while(!*stop) {
 		if (local_count != *count) {
@@ -52,19 +54,31 @@ void display(Mat* frame, int* count, bool* stop) {
 	}
 }
 
+
+int read_config() {
+	FILE *file;
+	YAML::Node config = YAML::LoadFile("config.yaml");
+	if (config["hello"]) {
+		std::cout << "YAML Test: " << config["hello"] << "\n";
+	}
+	return 0;
+}
+
 int main(int, char**) {
+	read_config();
+
 	Mat frame;
 	int shared_count = 0;
 	int count = 0;
 	bool stop = false;
 	std::thread t1 (cap_read, &frame, &shared_count, &stop);
-	std::thread t2 (display, &frame, &shared_count, &stop);
+	//std::thread t2 (display, &frame, &shared_count, &stop);
 	char img_name_buffer[100];
 	auto start = high_resolution_clock::now();
 	auto beginning = high_resolution_clock::now();
 	cout << setprecision(2) << fixed;
 	float fps = 0;
-	int fps_window = 10;
+	int fps_window = 5;
 	for (int i=0; i < 200; i++) {
 		if (count != shared_count) {
 			count = shared_count;
@@ -88,6 +102,6 @@ int main(int, char**) {
 	}
 	stop = true;
 	t1.join();
-	t2.join();
+	//t2.join();
 	return 0;
 }
--- cv_cap/README.md
+++ cv_cap/README.md
@@ -8,4 +8,4 @@ make
 ./DisplayImage
 ```
 
-
+Downlaod and install yaml-cpp from github. Known issue that *sudo apt install libyaml-dev* does not come with cmake support.
