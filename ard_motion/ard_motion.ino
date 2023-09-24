b8ae887c26dad4a289cb943a91dfdb3389b78c3a
andrew <andrewwong09@gmail.com>
2023-09-23 23:05:43 -0700

    serial com is not working completely

--- ard_motion/ard_motion.ino
+++ ard_motion/ard_motion.ino
@@ -1,9 +1,9 @@
-int pan_ena_pin = 2;
-int pan_dir_pin = 3;
-int pan_pul_pin = 4;
-int tilt_ena_pin = 7;
-int tilt_dir_pin = 8;
-int tilt_pul_pin = 9;
+int tilt_ena_pin = 2;
+int tilt_dir_pin = 3;
+int tilt_pul_pin = 4;
+int pan_ena_pin = 7;
+int pan_dir_pin = 8;
+int pan_pul_pin = 9;
 
 const int pulse_delay = 30;
 
@@ -21,16 +21,12 @@ void setup() {
   pinMode(tilt_ena_pin, OUTPUT);
   pinMode(tilt_dir_pin, OUTPUT);
   pinMode(tilt_pul_pin, OUTPUT);
-  digitalWrite(pan_ena_pin, HIGH); 
-  digitalWrite(tilt_ena_pin, HIGH); 
-  digitalWrite(LED_BUILTIN, HIGH);
-  drive(pan_pul_pin, pan_dir_pin, LOW, 50);
-  drive(pan_pul_pin, pan_dir_pin, HIGH, 50);
-  digitalWrite(LED_BUILTIN, LOW);
-  drive(tilt_pul_pin, tilt_dir_pin, LOW, 50);
-  drive(tilt_pul_pin, tilt_dir_pin, HIGH, 50);
+  digitalWrite(pan_ena_pin, LOW); 
+  digitalWrite(tilt_ena_pin, LOW); 
 }
 void drive(int pul_pin, int dir_pin, int dir, int num_pulses) {
+  digitalWrite(pan_ena_pin, HIGH);
+  digitalWrite(tilt_ena_pin, HIGH);
   digitalWrite(dir_pin, dir);
   for (int i=0; i < num_pulses; i++) {
     digitalWrite(pul_pin, HIGH);
@@ -38,6 +34,8 @@ void drive(int pul_pin, int dir_pin, int dir, int num_pulses) {
     digitalWrite(pul_pin, LOW);
     delay(pulse_delay);
   }
+  digitalWrite(pan_ena_pin, LOW);
+  digitalWrite(tilt_ena_pin, LOW);
 }
 
 motion_cmd get_command(void) {
