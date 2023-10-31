3f2fdd90a47d9b3a05a16f8a03f741c5347f0a92
andrew <andrew@neuralink.com>
2023-10-31 15:36:43 -0700

    enable/disable lid switch for Ubuntu 22.04 laptop.

---
+++ lid_switch/lid_reset.sh
@@ -0,0 +1,39 @@
+print_usage() {
+  echo "usage: ./lid_reset.sh -d <just flag to disable lid switch>"
+  exit 1
+}
+
+
+systemctl_reset() {
+    systemctl restart systemd-logind.service
+}
+
+
+enable_lid_switch() {
+    sudo cp logind_original.conf /etc/systemd/logind.conf
+    systemctl_reset
+}
+
+disable_lid_switch() {
+    sudo cp logind_disabled.conf /etc/systemd/logind.conf
+    systemctl_reset
+}
+
+
+no_args="true"
+while getopts "de" flag; do
+  case ${flag} in
+    d) disable_lid_swtich
+        ;;
+    e) enable_lid_swtich
+        ;;
+    *) print_usage
+      exit 1 ;;
+  esac
+  no_args="false"
+done
+
+if [ "$no_args" == "true" ]; then
+  print_usage
+fi
+
